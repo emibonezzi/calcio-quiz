6eee72c3ed4967e004f6d9409a144e2211fc8656
emibonezzi <emibonezzi@gmail.com>
2024-03-05 15:34:33 -0500

    Fix

--- src/services/api-client.tsx
+++ src/services/api-client.tsx
@@ -5,7 +5,7 @@ const axiosInstance = axios.create({
   baseURL: "https://api-football-v1.p.rapidapi.com/v3",
   headers: {
     "x-rapidapi-host": "api-football-v1.p.rapidapi.com/v3",
-    "x-rapidapi-key": process.env.VITE_API_KEY,
+    "x-rapidapi-key": import.meta.env.VITE_API_KEY,
   },
 });
 
